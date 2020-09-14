e2476bce245681ef3e378ed389a33c2c80e5833a
Juan Sebastian <seposorio@hotmail.com>
2020-09-14 14:21:58 -0500

    My Bookings - Last Bokings GUI responsive working

--- src/components/MyBookings.js
+++ src/components/MyBookings.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import "../css/MyBookings.css"
+import LastBookings from './LastBookings'
 
 function MyBookings() {
     return (
@@ -9,7 +10,34 @@ function MyBookings() {
                 <div className="mybookings__middleCont">
                     <h4>Reservas previas</h4>
                     <div className="mybookings__middleContBookings">
-
+                        <LastBookings
+                            active={true}
+                            day={24}
+                            month="Agosto"
+                            building="Edificio Reservas del Cedro"
+                            module="Modulo 1"
+                            hour="14:00 - 16:00" />
+                        <LastBookings
+                            active={false}
+                            day={15}
+                            month="Agosto"
+                            building="Edificio Reservas del Cedro"
+                            module="Modulo 1"
+                            hour="14:00 - 16:00" />
+                        <LastBookings
+                            active={false}
+                            day={12}
+                            month="Agosto"
+                            building="Edificio Reservas del Cedro el elefante feliz"
+                            module="Modulo 1"
+                            hour="14:00 - 16:00" />
+                        <LastBookings
+                            active={false}
+                            day={24}
+                            month="Agosto"
+                            building="Edificio Reservas del Cedro"
+                            module="Modulo 1"
+                            hour="14:00 - 16:00" />
                     </div>
                 </div>
                 <div className="mybookings__middleCont">
@@ -22,7 +50,6 @@ function MyBookings() {
                     <h4>Gasto total</h4>
                     <div className="mybookings__middleContTimeCost">
                         <h1>$100.000 <span>COP</span></h1>
-
                     </div>
                 </div>
             </div>
