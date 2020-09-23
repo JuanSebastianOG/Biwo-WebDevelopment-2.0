f5fbd9281187f2bd0e6de9ca9fba8554fec024da
Juan Sebastian <seposorio@hotmail.com>
2020-09-23 18:25:28 -0500

    Manage Start hour correctly (Firebase)
    Query all reservations on firebase given a day and select the correct hours to start a booking

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -1,11 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../css/Booking.css"
+import { db } from '../firebase';
 
 function Booking() {
+
+    const [date, setDate] = useState('')
+    var nonvalidHours = [];
+
+    const [startHours, setStartHours] = useState(["1. Ingrese Fecha"])
+    const AddStartHours = startHours.map(Add => Add)
+
+    const [endHours, setEndHours] = useState(["2. Ingrese hora inicio"])
+    const AddFinHours = endHours.map(Add => Add)
+
     const submitRegister = e => {
-        e.preventDefault();
     }
+
+    const dateChangeHandler = e => {
+        setDate(e.target.value);
+        var bookingRef = db.collection("reservas");
+        setStartHours(["1. Ingrese Fecha"])
+        setEndHours(["2. Ingrese hora inicio"])
+        //Takes all bookings on a specific dat ****NO FOR BUILD AND MODULE (NEED)****
+        var query = bookingRef.where("fecha", "==", e.target.value).get()
+            .then(function (querySnapshot) {
+                querySnapshot.forEach(function (doc) {
+                    // doc.data() is never undefined for query doc snapshots
+                    var startHours = doc.data().horaInicio;
+                    var endH = doc.data().horaFin;
+                    //For each booking registered, look for the non valid hours
+                    //And push it on the nonvalidHours array
+                    for (let i = 7; i < 22; i++) {
+                        if (i >= startHours && i < endH) {
+                            if (nonvalidHours.indexOf(i) === -1) {
+                                nonvalidHours.push(i);
+                                console.log(i);
+                            }
+                        }
+                    }
+                    console.log(nonvalidHours, 'nosepuede')
+                    console.log(doc.id, " => ", doc.data());
+                    /*Create the array and push all the valid hours 
+                    (the ones that does'nt exist on nonvalidHours array)*/
+                    let validHours = []
+                    validHours.push("-")
+                    for (let i = 7; i < 22; i++) {
+                        if (nonvalidHours.indexOf(i) === -1) {
+                            validHours.push(i)
+                        }
+                    }
+                    setStartHours(validHours)
+
+                });
+            })
+            .catch(function (error) {
+                console.log("Error getting documents: ", error);
+            });
+    }
+    const selectStartHourChangeHandler = e => {
+        let validEndHours = []
+        for (let i = startHours[e.target.value] + 1; i <= 22; i++) {
+            validEndHours.push(i)
+        }
+
+        setEndHours(validEndHours)
+
+        console.log(endHours);
+    }
+
     return (
+
         <div className="bking">
             <div className="bking__container">
                 <h1>Nueva Reserva</h1>
@@ -14,59 +78,25 @@ function Booking() {
                     <img className="bking_containerFlexImg" src="https://i.ibb.co/PG6R21D/Componente-1-1.png" alt="Componente-1-1" border="0" />
                     <form className="bking_containerFlexForm" action="">
                         <label htmlFor="">Fecha</label>
-                        <input type="date" />
+                        <input type="date" onChange={dateChangeHandler} />
                         <div className="bking_containerFlexFormHours">
                             <label htmlFor="">Hora Inicio</label>
                             <label>Hora Fin</label>
                         </div>
                         <div className="bking__containerTimes">
-                            <select id="out_time_hr" name="out_time_hr">
-                                <option value="0">00</option>
-                                <option value="1">01</option>
-                                <option value="2">02</option>
-                                <option value="3">03</option>
-                                <option value="4">04</option>
-                                <option value="5">05</option>
-                                <option value="6">06</option>
-                                <option value="7">07</option>
-                                <option value="8">08</option>
-                                <option value="9">09</option>
-                                <option value="10">10</option>
-                                <option value="11">11</option>
-                                <option value="12">12</option>
-                                <option value="13">13</option>
-                                <option value="14">14</option>
-                                <option value="15">15</option>
-                                <option value="16">16</option>
-                                <option value="17">17</option>
-                                <option value="18">18</option>
-                                <option value="19">19</option>
-                                <option value="20">20</option>
-                                <option value="21">21</option>
-                                <option value="22">22</option>
-                                <option value="23">23</option>
 
+                            <select id="in_time_hr" name="out_time_hr" onChange={selectStartHourChangeHandler}>
+                                {
+                                    AddStartHours.map((hourStart, key) => <option value={key}>{hourStart}</option>)
+                                }
                             </select>
                             <h1> - </h1>
-                    
-                            <select className="bking__time" id="out_time_hr" name="out_time_hr">
-
-                                <option value="8">08</option>
-                                <option value="9">09</option>
-                                <option value="10">10</option>
-                                <option value="11">11</option>
-                                <option value="12">12</option>
-                                <option value="13">13</option>
-                                <option value="14">14</option>
-                                <option value="15">15</option>
-                                <option value="16">16</option>
-                                <option value="17">17</option>
-                                <option value="18">18</option>
-                                <option value="19">19</option>
-
-
+                            <select value={endHours} className="bking__time" id="out_time_hr" name="out_time_hr">
+                                {
+                                    AddFinHours.map((hourFin, key) => <option value={key}>{hourFin}</option>)
+                                }
                             </select>
-                            
+
                         </div>
                         <div className="bking__containerButton">
 
@@ -78,7 +108,7 @@ function Booking() {
                     </form>
                     <div></div>
                 </div>
-                
+
 
             </div>
 
