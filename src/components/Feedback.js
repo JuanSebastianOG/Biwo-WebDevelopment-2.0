f47e40608dd07c6cf011145267a66036efc7323f
Abel Cortes <abelsantiagocortes@gmail.com>
2021-01-14 09:18:13 +1100

    Feedback Working

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -3,8 +3,10 @@ import { Container } from "reactstrap"
 import styled from 'styled-components';
 import ReactStars from "react-rating-stars-component";
 import "../css/Help.css"
-import {  db } from '../firebase';
+import { db } from '../firebase';
 import { useState } from 'react'
+import NavBar from './NavBar';
+import { useHistory } from "react-router-dom";
 
 import "../css/Help.css"
 
@@ -26,7 +28,7 @@ const StyledContainer = styled(Container)`
     justify-content: space-evenly;
 }`
 
-const StyledTitle1= styled.h1`
+const StyledTitle1 = styled.h1`
  margin-top:40px;
     margin-left:80px;
     color: #002980;
@@ -35,7 +37,9 @@ const StyledTitle1= styled.h1`
 `;
 
 
-const Feedback = () => {
+const Feedback = ({ match }) => {
+
+    const history = useHistory();
 
     const [feedback, setFeedback] = useState('')
     const [ratingService, setRatingService] = useState('')
@@ -55,27 +59,45 @@ const Feedback = () => {
     };
 
     const sendFeedback = (e) => {
-        
+        console.log(match.params.idBooking)
         e.preventDefault();
-        db.collection("reseñas").add({
-            ratingServicio: ratingService,
-            ratingInternet: ratingInternet,
-            ratingLimpieza:ratingNeatness,
-            mensaje: feedback
-        }).then(function (docRef) {
-            alert("Su reseña ha sido enviado existosamente")
-        }).catch(function (error) {
-                console.error("Error adding document: ", error);
-        });
-     
-        
+        db.collection('reseñas')
+        .where("idBooking", "==", match.params.idBooking)
+        .get()
+        .then(function (querySnapshot) {
+            if (querySnapshot.docs.length == 0) {
+                db.collection("reseñas")
+                .add({
+                    idBooking: match.params.idBooking,
+                    ratingServicio: ratingService,
+                    ratingInternet: ratingInternet,
+                    ratingLimpieza: ratingNeatness,
+                    mensaje: feedback
+                }).then(function (doc) {
+                    alert("Su reseña ha sido enviado exitosamente")
+                    history.push("/misReservas");
+                }).catch(function (error) {
+                    console.error("Error adding document: ", error);
+                });
+
+               
+
+
+            } else {
+                alert("Ya se realizo una reseña sobre esta reserva")
+                    history.push("/misReservas");
+            }
+        })
     }
+
+
 
     return (
         <div>
+            <NavBar users active="misreservas" usertype={"residente"} />
             <StyledTitle1>Reseña de Reserva</StyledTitle1>
             <StyledContainer >
-                
+
                 <StyledTitle >Satisfacción General Del Servicio</StyledTitle>
                 <ReactStars
                     count={5}
@@ -97,13 +119,13 @@ const Feedback = () => {
                     size={30}
                     activeColor="#ffd700"
                 />
-                                <StyledTitle >Comentarios Adicionales</StyledTitle>
+                <StyledTitle >Comentarios Adicionales</StyledTitle>
 
                 <form action="" className="help__containerFlexRight">
 
                     <textarea value={feedback} onChange={(e) => setFeedback(e.target.value)} rows="2" cols="10" />
                     <button onClick={sendFeedback}>Enviar Reseña</button>
-                    
+
                 </form>
             </StyledContainer>
 
